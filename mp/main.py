36f823b6d6fa421c0c41f60064325663b8c97dbd
Nick <nick@debian-BULLSEYE-live-builder-AMD64>
2021-12-04 23:02:38 -0700

    added radius knob

--- mp/main.py
+++ mp/main.py
@@ -1,29 +1,56 @@
 import time
-from math import cos, sin, pi
+from math import isinf
+from micropython import const
 from rotary_irq_esp import RotaryIRQ
-from claw import Claw
+from claw import Claw, time_it
 
 
-
-if __name__ == "__main__":
+def main():
     claw = Claw(22)
 
-    knob = RotaryIRQ(pin_num_clk=21,
-                    pin_num_dt=19,
-                    min_val=-20,
-                    max_val=20,
-                    reverse=True,
-                    range_mode=RotaryIRQ.RANGE_BOUNDED)
-    knob.set(value=0)
+    speedKnob = RotaryIRQ(pin_num_clk=21,
+                        pin_num_dt=19,
+                        min_val=-13,
+                        max_val=13,
+                        reverse=True,
+                        range_mode=RotaryIRQ.RANGE_BOUNDED)
+    speedKnob.set(value=7)
+
+
+    radKnob = RotaryIRQ(pin_num_clk=23,
+                        pin_num_dt=18,
+                        min_val=4, # will be * 5
+                        max_val=30,
+                        reverse=True,
+                        range_mode=RotaryIRQ.RANGE_BOUNDED)
+    radKnob.set(value=12) # 60mm radius
+    
+
+    speedTable = {
+    #speedKnob: numFrames
+        0:     float('inf'), # stopped
+        1:         80,       # slowest
+        2:         75,
+        3:         70,
+        4:         65,
+        5:         60,
+        6:         55,
+        7:         50,       # default
+        8:         45,
+        9:         40,
+        10:        35, 
+        11:        30,
+        12:        25,
+        13:        20        # fastest
+    }  
     
     
     STRIDE = 1.15               # radians, must be less than 2*pi/5
     OFFSET_ANGLE = -STRIDE / 2  # radians
-    FRAME_TIME = 60             # ms
+    FRAME_TIME = const(60)      # ms
     
-    radius = 60
     center = [0, 0, 150]        # center of the circle that the finger tips move along
-    numFrames = -50             # multiple of 5
+    radius = radKnob.value() * 5
     phase = 0                   # sweeps 0-359
     frameStartTime = time.ticks_ms()
     firstLoop = True
@@ -31,17 +58,55 @@ if __name__ == "__main__":
 
     ### MAIN LOOP ###
     while True:
-        # Calculate angles of all 15 servos for next frame
-        angles = claw.calc_frame_angles(phase, STRIDE, OFFSET_ANGLE, radius, center)
-
-        # ------Wait------
-        while time.ticks_diff(time.ticks_ms(), frameStartTime) < FRAME_TIME:
-            pass
-        frameStartTime = time.ticks_ms() # reset timer
+        # Update values based on user input
+        phase, numFrames, radius = update_values(phase, speedKnob, radKnob, speedTable, radius)
         
-        # Move all fingers to frame
-        claw.move_to_frame(angles, FRAME_TIME, firstLoop)
-        firstLoop = False
+        # If not stopped
+        if not isinf(numFrames):
+            # Calculate angles of all 15 servos for next frame
+            angles = claw.calc_frame_angles(phase, STRIDE, OFFSET_ANGLE, radius, center)
 
-        # Increment phase
-        phase = (phase + 360 / numFrames) % 360
+            # ------Wait------
+            while time.ticks_diff(time.ticks_ms(), frameStartTime) < FRAME_TIME:
+                pass
+            frameStartTime = time.ticks_ms() # reset timer
+            
+            # Move all fingers to frame
+            claw.move_to_frame(angles, FRAME_TIME, firstLoop)
+            firstLoop = False
+
+            # Increment phase
+            phase = (phase + 360 / numFrames) % 360
+
+
+@time_it
+def update_values(phase, speedKnob, radKnob, speedTable, radius):
+    """Update values based on user input"""
+
+    # speed knob position --> number of frames
+    speedKnobVal = speedKnob.value()
+    if speedKnobVal >= 0:
+        numFrames = -speedTable[speedKnobVal]
+    else:
+        numFrames = speedTable[-speedKnobVal] # Negative number of frames reverses direction
+    
+    
+    # If not stopped
+    if not isinf(numFrames):
+        multiple = 360 / (-numFrames)
+        phase = multiple * round(phase / multiple) # round phase to nearest multiple
+
+        # radius knob position --> radius
+        radius = radKnob.value() * 5
+    
+    # If stopped
+    else:
+        # Don't allow changing radius
+        radKnob.set(value=int(radius/5))
+
+
+    return phase, numFrames, radius
+
+
+if __name__ == "__main__":
+    main()
