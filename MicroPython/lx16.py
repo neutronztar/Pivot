4210602cb21133d5f151d87a27b6431348e92d67
Nick <ndwallick@gmail.com>
2022-01-21 23:57:38 -0700

    added license to lx16.py

--- MicroPython/lx16.py
+++ MicroPython/lx16.py
@@ -1,6 +1,29 @@
-# This single-file library was originally made by sasilva1998 and then modified by Nick Wallick
+# This single-file library was originally made by sasilva1998 under the MIT license and then modified by Nick Wallick
 # Original library https://github.com/FunPythonEC/uPy_Lewansoul_LX-16
 # Used to send and recieve commands to and from the LewanSoul LX-16A servos
+
+
+# MIT License
+
+# Copyright (c) 2019 FunPython
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 import machine as m    # needed so that uart can be used
 from micropython import const
